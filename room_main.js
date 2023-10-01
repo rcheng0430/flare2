a0649edc08b7d4aba321e733d464b947f1e3fda7
rcheng0430 <123599083+rcheng0430@users.noreply.github.com>
2023-09-30 18:58:59 -0700

    Add files via upload

---
+++ room_main.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC_8r98AP8ZPe9F7D6IktEquea14w-pMSA",
+    authDomain: "flare-main.firebaseapp.com",
+    databaseURL: "https://flare-main-default-rtdb.firebaseio.com",
+    projectId: "flare-main",
+    storageBucket: "flare-main.appspot.com",
+    messagingSenderId: "786263422815",
+    appId: "1:786263422815:web:564ee72a99905a5e9aa8ea"
+  };
+  
+  
+  firebase.initializeApp(firebaseConfig);
